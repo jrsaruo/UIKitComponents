3f179c807b8bd1fb17ecdf2830e0faa6d9642648
Yusaku Nishi <jrsaruo@gmail.com>
2023-02-19 15:50:08 +0900

    change: use collectionView instead of tableView to display samples

--- Demo/UIKitComponentsDemo/TopViewController.swift
+++ Demo/UIKitComponentsDemo/TopViewController.swift
@@ -10,26 +10,46 @@ import SwiftyTable
 
 final class TopViewController: UIViewController {
     
-    private let tableView: UITableView = {
-        let tableView = UITableView(frame: .zero, style: .insetGrouped)
-        tableView.register(UITableViewCell.self)
-        return tableView
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewCompositionalLayout { sectionIndex, layoutEnvironment in
+            let columnCount = Int(layoutEnvironment.container.contentSize.width / 160)
+            let group = NSCollectionLayoutGroup.horizontal(
+                layoutSize: .init(
+                    widthDimension: .fractionalWidth(1),
+                    heightDimension: .estimated(140)
+                ),
+                repeatingSubitem: .init(layoutSize: .init(
+                    widthDimension: .fractionalWidth(1 / CGFloat(columnCount)),
+                    heightDimension: .estimated(140)
+                )),
+                count: columnCount
+            )
+            group.interItemSpacing = .fixed(16)
+            
+            let section = NSCollectionLayoutSection(group: group)
+            section.contentInsetsReference = .layoutMargins
+            section.interGroupSpacing = 16
+            return section
+        }
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .systemGroupedBackground
+        collectionView.contentInset.top = 16
+        collectionView.register(SampleCollectionViewCell.self)
+        return collectionView
     }()
     
-    private lazy var dataSource = UITableViewDiffableDataSource<Int, Sample>(tableView: tableView) { tableView, indexPath, sample in
-        let cell = tableView.dequeueReusableCell(of: UITableViewCell.self, for: indexPath)
-        cell.accessoryType = .disclosureIndicator
-        var configuration = cell.defaultContentConfiguration()
-        configuration.text = sample.title
-        cell.contentConfiguration = configuration
+    private lazy var dataSource = UICollectionViewDiffableDataSource<Int, Sample>(collectionView: collectionView) { collectionView, indexPath, sample in
+        let cell = collectionView.dequeueReusableCell(of: SampleCollectionViewCell.self, for: indexPath)
+        cell.configure(title: sample.title)
         return cell
     }
     
     // MARK: - Lifecycle
     
     override func loadView() {
-        tableView.delegate = self
-        view = tableView
+        collectionView.delegate = self
+        view = collectionView
     }
     
     override func viewDidLoad() {
@@ -48,15 +68,15 @@ final class TopViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.deselectSelectedRow(with: transitionCoordinator, animated: animated)
+        collectionView.deselectSelectedItems(with: transitionCoordinator, animated: animated)
     }
 }
 
-// MARK: - UITableViewDelegate -
+// MARK: - UICollectionViewDelegate -
 
-extension TopViewController: UITableViewDelegate {
+extension TopViewController: UICollectionViewDelegate {
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let sample = dataSource.itemIdentifier(for: indexPath)!
         navigationController?.pushViewController(sample.destination, animated: true)
     }
@@ -64,7 +84,7 @@ extension TopViewController: UITableViewDelegate {
 
 extension TopViewController {
     
-    enum Sample: Int, TableRow, CaseIterable {
+    enum Sample: Int, CollectionItem, CaseIterable {
         case textViewWithPlaceholder
         
         var title: String {
